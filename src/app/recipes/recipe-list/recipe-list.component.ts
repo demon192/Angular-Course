0fb99c1f5982fba914dcc82e86fa4806d2f335b6
Ishu Mehra <ishumehra1534@gmail.com>
2023-05-23 17:18:56 +0530

    feature/databinding_and_components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { EventEmitter, Output, Component } from '@angular/core';
 import { Recipe } from '../recipes.model';
 
 @Component({
@@ -7,8 +7,12 @@ import { Recipe } from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe','this is a test', 'https://www.thespruceeats.com/thmb/JtOA_LnrhrW_TfS3MYCqlPqhUFA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/basic-congee-recipes-4065244-hero-01-5cf81547c34d4520be10bb57c6cda902.jpg'),
-    new Recipe('A test recipe','this is a test', 'https://www.thespruceeats.com/thmb/JtOA_LnrhrW_TfS3MYCqlPqhUFA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/basic-congee-recipes-4065244-hero-01-5cf81547c34d4520be10bb57c6cda902.jpg')  ];
+    new Recipe('Another test recipe','this is a test', 'https://www.thespruceeats.com/thmb/JtOA_LnrhrW_TfS3MYCqlPqhUFA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/basic-congee-recipes-4065244-hero-01-5cf81547c34d4520be10bb57c6cda902.jpg')  ];
 
+    onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
+    }
 }
